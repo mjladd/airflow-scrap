3b51df9afc90cddb35d878b90ff9ae683f6c6d49
Michael Ladd <mjladd@gmail.com>
2021-12-16 20:31:17 -0600

    adding wiki pageview

--- dags/download_wikipedia_pageiviews.py
+++ dags/download_wikipedia_pageiviews.py
@@ -3,14 +3,14 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 
 dag = DAG(
-    dag_id = "wiki_pageview",
-    start_date = airflow.utils.dates.days_ago(3),
-    schedule_interval='@hourly'
+    dag_id="wiki_pageview",
+    start_date=airflow.utils.dates.days_ago(3),
+    schedule_interval="@hourly",
 )
 
 get_data = BashOperator(
-    task_id = "get_data",
-    bash_command = (
+    task_id="get_data",
+    bash_command=(
         "curl -o /tmp/wikipageviews.gz "
         "https://dumps.wikimedia.org/other/pageviews/"
         "{{ execution_date.year }}/"
@@ -19,6 +19,5 @@ get_data = BashOperator(
         "{{ '{:02}'.format(execution_date.day) }}-"
         "{{ '{:02}'.format(execution_date.hour) }}0000.gz"
     ),
-    dag = dag,
+    dag=dag,
 )
-
